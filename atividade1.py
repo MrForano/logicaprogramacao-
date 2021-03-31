b994786a72dc467946db539d920bdf0b35405ae4
Forano <52356147+MrForano@users.noreply.github.com>
2021-03-31 19:31:20 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,1 +1,55 @@
-print("Olá professor")
+soma = 0
+list = [1, 1]
+
+
+def calculate_soma(i):
+  i = numbers
+
+
+for soma in list:
+  soma += int(soma)
+
+print(soma)
+
+
+i = "2 5 1 4 8"
+numbers = i.split()
+list = []
+
+
+def converter_entrada(numbers):
+  for number in numbers:
+
+    list.append(int(number))
+  return list
+
+
+list = converter_entrada(numbers)
+print(list)
+
+i = [2, 6, 1, 3]
+val1 = 0
+val2 = len(i)
+
+
+def process_numbers(numbers):
+  i = numbers
+
+
+for numbers in i:
+    val1 += int(numbers)
+print(val1)
+print(val2)
+
+
+def main(numbers):
+  i = soma_index[1]
+  soma = soma_index[0]
+  media = soma/i
+  return media
+
+
+list = converter_entrada(input())
+soma_index = process_numbers(list)
+media = main(list)
+print(media)
