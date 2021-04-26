704875b7fbd202caadf0e2d152616172b4be675a
Forano <52356147+MrForano@users.noreply.github.com>
2021-04-26 12:35:15 -0300

    Create atividade12.py

---
+++ atividade12.py
@@ -0,0 +1,39 @@
+num1 = int(input('Insira um numero: '))
+num2 = int(input('Insira um numero: '))
+num3 = int(input('Insira um numero: '))
+
+number_list = [num1, num2, num3]
+max(number_list)
+print(max(number_list))
+
+print(' ')
+print('---------------------------------------')
+print(' ')
+
+li = list(" ")
+
+
+str1 = "Eu sou um aluno feliz"
+print(str1)
+
+print(' ')
+print('---------------------------------------')
+print(' ')
+
+list = [1, 2, 3, 4]
+list_sum = sum(list)
+print(list_sum)
+
+print(' ')
+print('---------------------------------------')
+print(' ')
+
+s = ["Seu", "João", "esta", "aqui"]
+
+phrase_list = ' '.join([str(elem) for elem in s])
+
+print(phrase_list)
+
+print(' ')
+print('---------------------------------------')
+print(' ')
